fe1012b91e4cef4762286602a28e66d0baa2d062
Samir <samir.sattorov01@mail.ru>
2024-07-15 14:21:32 +0300

    finished lesson-1

---
+++ script.js
@@ -0,0 +1,54 @@
+// console.log("Hello world");
+// var message = "Hello";
+// var lg = "televizor";
+// let lg = "televisor";
+// lg = "кереги жок буюмдар";
+// console.log(lg);
+
+// const samsung = "televisor";
+
+// samsung = "dividi";
+
+// console.log(samsung);
+
+const number = 4;
+const numberDoc = document.getElementById("type");
+
+numberDoc.textContent;
+let text = "something text";
+//2)
+//3)
+//4)
+//5) Undefined
+
+console.log(number);
+
+// var number = 5;
+
+//6) Symbol
+
+let numbers = 1000000000;
+console.log(numbers);
+
+// 8) Object
+// const person = {
+//     name: "Samir",
+//     age: "13",
+//     color:"red",
+// }
+
+let num1 = 7;
+let num2 = 3;
+let result = num1 - num2;
+
+console.log(result);
+
+let message1 = "Hello";
+let message2 = "World";
+
+let combinedMessage = `${message1} ${message2}`;
+
+const results = confirm("сиз 18 ге чыкансызбы ? ");
+console.log(result);
+let userName = prompt("Как ваше имя ?");
+console.log(userName);
