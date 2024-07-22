2cd87faa1b140799730ad98c26dec12bd5a64b56
Samir <samir.sattorov01@mail.ru>
2024-07-22 14:35:34 +0300

    j

---
+++ script.js
@@ -0,0 +1,44 @@
+// let num = 5;
+// function addNumbers(num) {
+//   const result = 15 + 5;
+//   console.log(result);
+// }
+
+// // function getNumber() {
+// //   for (let i = 0; i <10)
+// // }
+
+// addNumbers(5, 10);
+
+// function addNumber() {
+//   let result = -1;
+//   result++;
+//   console.log(result);
+// }
+
+// addNumber();
+// addNumber();
+// addNumber();
+
+// const squareOf2 = square(2);
+// console.log(squareOf2);
+
+// const squareOf3 = square(5);
+// console.log(squareOf3);
+
+// function calc() {
+//   const result = 6 + 9;
+//   return result
+// }
+// const nooruz = calc();
+// console.log(nooruz);
+
+function square(num, num2) {
+  const result = num ** num2;
+  return result;
+}
+console.log(square);
+
+const getTextOfLength = (text) => text.length;
+const result2 = getTextOfLength("Samir");
+console.log(result2);
