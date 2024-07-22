155a00ef1ea685c45990b6008d302b4ea4cc4203
Samir <samir.sattorov01@mail.ru>
2024-07-22 12:10:00 +0300

    i

--- script.js
+++ script.js
@@ -31,32 +31,64 @@ console.log(b);
 
 // //! task 1
 
-// let index2 = 0;
+let index2 = 0;
 
-// while (index2 <= 100) {
-//   console.log(index2);
-//   index2++;
-// }
+while (index2 <= 100) {
+  console.log(index2);
+  index2++;
+}
 
 //! task 2
 
-// let counter = 0;
+let counter = 0;
 
-// while (counter <= 100) {
-//   if (counter % 2 === 0) {
-//     console.log(counter);
-//   }
-//   counter++;
-// }
+while (counter <= 100) {
+  if (counter % 2 === 0) {
+    console.log(counter);
+  }
+  counter++;
+}
 
 //! task 3
 
 let counter2 = 6;
 let result = 15;
 
-while (counter2 <= 10) {
+while (counter2 <= 100) {
   result = result + counter2;
   counter2 = counter2 + 1;
 }
 
 console.log(result);
+
+let text = "Nooruz";
+
+//! length тесктиин узундугун эсептеп берет
+console.log(text.length);
+
+//! charAt()
+console.log(text.charAt());
+
+let counter3 = 0;
+
+while (0 < counter3) {
+  console.log(text.charAt(counter3));
+  counter3--;
+}
+
+let text2 = prompt("Какой сегодня день недели?");
+
+switch (text2) {
+  case "Понедельник":
+  case "Вторник":
+  case "Среда":
+  case "Четверг":
+  case "Пятница":
+    alert("обаа туура");
+    break;
+
+  case "Суббота":
+  case "Воскресение": {
+    alert("жок туур эмес");
+  }
+}
