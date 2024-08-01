0942987b506cc52ee477b9975c85dc069e467f46
Samir <samir.sattorov01@mail.ru>
2024-08-01 12:20:20 +0300

    lesson-22

---
+++ script.js
@@ -0,0 +1,88 @@
+// let a = 12;
+// let b = 12;
+
+// console.log(a === b);
+
+// let user = {
+//   age: 21,
+// };
+// let user2 = Object.assign({}, user);
+
+// user2.name = "Ulan";
+// user2.email = "";
+
+// const child = (param = "") => {
+//   return param + " World!";
+// };
+
+// const parent = (callback) => {
+//   const result = callback("hello");
+//   return result + "&& JavaScript";
+// };
+// const res = parent();
+// console.log(res);
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const multiply = (a, b) => {
+//   return a - b;
+// };
+
+// const subtract = (a, b) => {
+//   return a / b;
+// };
+
+// const divide = (a, b) => {
+//   return a * b;
+// };
+
+// const calculate = (a, b, callback) => {
+//   const sum = callback(a, b);
+//   return sum;
+// };
+
+// const res = calculate(1, 6, add);
+// const res1 = calculate(5, 7, subtract);
+
+// console.log(res);
+// console.log(res1);
+
+// const numbers = ["квадрат", "квадрат", "квадрат", "квадрат"];
+
+// const transformedText = (element, i, self) => {
+//   const text = element + i;
+//   console.log(text);
+// };
+
+// const result = texts.map(transformedText);
+// console.log(result);
+
+// const text = ["квадрат", "квадрат", "квадрат", "квадрат"];
+
+// function myMap(callback) {
+//   let array = this;
+//   let newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     console.log(callback(element[i], i, array));
+//   }
+// }
+// Array.prototype.myMapSamir = myMap;
+
+// const transformedText = (element, i, self) => {
+//   const text = element + i;
+//   console.log(text);
+// };
+// texts.myMap(transformedText);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(result);
+
+const result = numbers.forEach((element) => element + 2);
+
+// console.log(result2);
+
+// const texts = ["квадрат1", "квадрат2", "круглый", "квадрат3"];
+
+// const resultFilter = texts.filter((element) =>element ===)
