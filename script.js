a3165697790e1aba9e690d772828c1e5b2d8e416
Samir <samir.sattorov01@mail.ru>
2024-08-02 12:30:22 +0300

    lesson-23

---
+++ script.js
@@ -0,0 +1,38 @@
+// let num = 5;
+// let string = "asdasasda";
+
+// function addNumbers(num1, num2) {
+//   let sum = 0;
+//   num1 >= 5 ? (sum = num1 + num2) : (sum = num1 - num2);
+//   return sum;
+// }
+
+// const res = addNumbers(4, 25);
+// console.log(res);
+
+// const addNumbers1 = function () {
+
+// }
+
+// const users = [
+//   { name: "Samir", surname: " Sattorov", age: 13, city: "Moscow" },
+//   { name: "Amir", surname: " Sattorov", age: 14, city: "Batken" },
+//   { name: "Zamir", surname: " Sattorov", age: 11, city: "Osh" },
+//   { name: "Damir", surname: " Sattorov", age: 12, city: "Batken" },
+// ];
+
+// const newUsers = users.map((item, i) => {
+//   return {
+//     id: i + 1,
+//     name: item.name,
+//     age: item.age,
+//     city: item.city,
+//   };
+// });
+// const deleteObject = newUsers.filter((item) => {
+//   if (item.id !== 3) {
+//     return true;
+//   }
+//   return false;
+// });
+// console.log(deleteObject);
