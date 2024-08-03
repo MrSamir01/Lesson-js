275bb7dda031f777c3cc8a17d7f4680fce81fd85
Samir <samir.sattorov01@mail.ru>
2024-08-03 13:02:19 +0300

    bla bla

---
+++ script.js
@@ -0,0 +1,39 @@
+// const array = ["Samir", "Zamir", "Damir", "Amir"];
+// const sortirovka = array.sort();
+// console.log(sortirovka);
+
+// const array2 = [1, 2, 3, 4, 6, 5];
+// const sortirovka2 = array2.sort((prev, next) => next - prev);
+// console.log(sortirovka2);
+
+// const user = {
+//   name: "Samir",
+//   age: 13,
+//   city: "Moscow",
+// };
+
+// const userKeys = Object.keys(user);
+// console.log(userKeys);
+
+// const date = new Date();
+// console.log(date);
+
+// const minute = date.getMinutes();
+// console.log(minute);
+
+// const seconds = date.getSeconds();
+// console.log(seconds);
+
+// const hours = date.getHours();
+// console.log(hours);
+
+// console.log(`${hours}:0${minute}:0${seconds}`);
+
+// const firstName = "Aisuluu";
+// const lastName = "Bekeeva";
+
+const number = 90.5;
+
+console.log(Math.ceil(number), "ceil");
+console.log(Math.floor(number), "floor");
+console.log(Math.round(number), "round");
