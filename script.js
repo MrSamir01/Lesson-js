3ba2d96f5c910c51e810cbc3c19de7c2372ef519
Samir <samir.sattorov01@mail.ru>
2024-08-16 12:15:27 +0300

    lesson-29

---
+++ script.js
@@ -0,0 +1,49 @@
+// setTimeout(() => {
+//   alert("ChupaChoops");
+// }, 3000);
+
+// setInterval(() => {
+//   alert("Прошло 2 секунды");
+// }, 2000);
+
+// function a(num) {
+//   if (num < 10) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+
+// setTimeout(() => a(4), 5000);
+
+// function a(num, num1) {
+//   console.log(num1, num);
+//   console.log(num1 + num);
+// }
+// setTimeout(a, 1000, 5, 5);
+
+// const res = setInterval(() => {
+//   console.log("setInterval");
+// }, 2000);
+
+// setTimeout(() => {
+//   clearInterval(res);
+// }, 5000);
+
+// console.log(1)
+// setInterval(())
+// console.log(3)
+
+// const promise = new Promise((resolve, rejected) => {
+//   resolve("Hello JS-3");
+//   rejected("Not Hello");
+// });
+
+// promise
+// .then((message) => {
+//   console.log(message);
+// });
+
+// .catch(error) => {
+//   console.log
+// }
